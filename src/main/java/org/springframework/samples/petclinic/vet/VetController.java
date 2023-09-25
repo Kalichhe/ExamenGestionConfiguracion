5301428647ea7993492ed19f847dca5d0c8f9f9c
Kalichhe <jhondh2003@gmail.com>
2023-09-25 14:13:06 -0500

    Subiendo codigo

---
+++ src/main/java/org/springframework/samples/petclinic/vet/VetController.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.samples.petclinic.vet;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.samples.petclinic.owner.Owner;
+import org.springframework.samples.petclinic.owner.Pet;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author Juergen Hoeller
+ * @author Mark Fisher
+ * @author Ken Krebs
+ * @author Arjen Poutsma
+ */
+@Controller
+class VetController {
+
+	private final VetRepository vets;
+
+	public VetController(VetRepository vets) {
+		this.vets = vets;
+	}
+
+	@ModelAttribute("vet")
+	public Vet findOwner(@PathVariable(name = "vetId", required = false) Integer vetId) {
+		return vetId == null ? new Vet()
+				: this.vets.findById(vetId).isPresent() ? this.vets.findById(vetId).get() : null;
+	}
+
+	@GetMapping("/vets.html")
+	public String showVetList(@RequestParam(defaultValue = "1") int page, Model model) {
+		System.out.println("page: " + page);
+		// Here we are returning an object of type 'Vets' rather than a collection of Vet
+		// objects so it is simpler for Object-Xml mapping
+		Vets vets = new Vets();
+		Page<Vet> paginated = findPaginated(page);
+		vets.getVetList().addAll(paginated.toList());
+		return addPaginationModel(page, paginated, model);
+	}
+
+	private String addPaginationModel(int page, Page<Vet> paginated, Model model) {
+		List<Vet> listVets = paginated.getContent();
+		model.addAttribute("currentPage", page);
+		model.addAttribute("totalPages", paginated.getTotalPages());
+		model.addAttribute("totalItems", paginated.getTotalElements());
+		model.addAttribute("listVets", listVets);
+		model.addAttribute("vetId", 1);
+		return "vets/vetList";
+	}
+
+	private Page<Vet> findPaginated(int page) {
+		int pageSize = 5;
+		Pageable pageable = PageRequest.of(page - 1, pageSize);
+		return vets.findAll(pageable);
+	}
+
+	@GetMapping({ "/vets" })
+	public @ResponseBody Vets showResourcesVetList() {
+		System.out.println("showResourcesVetList");
+		// Here we are returning an object of type 'Vets' rather than a collection of Vet
+		// objects so it is simpler for JSon/Object mapping
+		Vets vets = new Vets();
+		vets.getVetList().addAll(this.vets.findAll());
+		return vets;
+	}
+
+	/**
+	 * Custom handler for displaying an owner.
+	 * @param vetId the ID of the vet to display
+	 * @return a ModelMap with the model attributes for the view
+	 */
+	@GetMapping("/vets/{vetId}")
+	public ModelAndView showOwner(@PathVariable("vetId") int vetId) {
+		ModelAndView mav = new ModelAndView("vets/vetDetails");
+		Vet vet = this.vets.findById(vetId).orElse(null);
+		mav.addObject(vet);
+		return mav;
+	}
+
+}
